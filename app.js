4b05e9a00c112a95ab92965a9bbc931be24c86b0
TarunBits <tarunbhattmortal@gmail.com>
2025-11-05 12:52:23 +0530

    Typescript setup

---
+++ app.js
@@ -0,0 +1,7 @@
+var userName = "Kartik Aryan";
+var age = 20;
+var college = "IET alwar";
+
+console.log(userName);
+console.log(age);
+console.log(college);
---
+++ app.ts
@@ -0,0 +1,7 @@
+var userName: string = "Kartik Aryan";
+var age: number = 20;
+var college: string = "IET alwar";
+
+console.log(userName);
+console.log(age);
+console.log(college);
